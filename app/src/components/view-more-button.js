69367a8b22576ee28b8afdcfc91380276008616f
Little Projects <>
2024-04-28 15:19:49 +0100

    Creating test button component

---
+++ app/src/components/view-more-button.js
@@ -0,0 +1,11 @@
+import './../assets/styles/index.css';
+
+function ViewMoreCTA() {
+  return (
+      <button id="more">
+        View More
+      </button>
+    )
+}
+
+export default ViewMoreCTA;
