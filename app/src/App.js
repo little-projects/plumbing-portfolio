944357c8b91a7948ebedc37ff507f1daa638937f
little-projects <eoconn004@gmail.com>
2024-05-27 16:43:39 +0100

    Menu switches between pages

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,48 @@
+import { useState } from 'react'
+
+// Pages
+import About from './pages/about'
+import Brands from './pages/brands'
+import Contact from './pages/contact'
+
 // Components
 import ViewMoreCTA from './components/view-more-button'
-import NavMenu from './components/menu'
-
-// Page being worked on
-import Contact from './pages/contact'
 
 // Styling
 import './assets/styles/index.css';
 
+const App = () => {
+  const [currentPage, setCurrentPage ] = useState('about')
+  
+  const renderPage = () => {
+    switch(currentPage) {
+      case 'brands':
+        return <Brands/>
+      case 'contact':
+        return <Contact/>
+      case 'about':
+      default:
+        return <About/>
+    }
+  }
 
-
-function App() {
   return (
-      <div>
-          <h1>Welcome</h1>
-          <NavMenu/>
-          <Contact/>
-          <ViewMoreCTA/>
-      </div>
-    )
+    <div>
+        <h1>Welcome</h1>
+        <nav>
+          <ul>
+            <li> <button onClick={() => setCurrentPage('about')}>About </button></li>
+            <li> <button onClick={() => setCurrentPage('brands')}>Brands </button></li>
+            {/* <li> <button onClick={() => setCurrentPage('')}>Services </button></li> */}
+            {/* <li> <button onClick={() => setCurrentPage('')}>Portfolio </button></li> */}
+            <li> <button onClick={() => setCurrentPage('contact')}>Contact </button></li>
+            {/* <li> <button onClick={() => setCurrentPage('')}>Reviews </button></li> */}
+          </ul>
+        </nav>
+
+        {renderPage()}
+    </div>
+  )
 }
 
 export default App;
---
+++ app/src/pages/brands.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Brands extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <h2>Brands</h2>
+        )
+    }
+}
+
+export default Brands
